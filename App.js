b29e57b18f4931cf1ae38fb3902027ac7606ad5c
Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>
2021-11-05 00:19:36 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,28 +1,28 @@
 import React, { useState } from "react";
-import { Switch, Text, TextInput, View } from "react-native";
 
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
 import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingScreen from "./app/screens/ListingScreen";
+import AppPicker from "./app/components/AppPicker";
 import AppTextInput from "./app/components/AppTextInput";
+
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+];
+
 export default function App() {
-  // const [firstName, setFirstName] = useState("");
-  // console.log(firstName)
-  const [isNew, setIsNew] = useState(false);
+  const [category, setCategory] = useState(categories[0]);
+
   return (
     <Screen>
-      <Switch
-        value={isNew}
-        onValueChange={(newValue) => {
-          setIsNew(newValue);
-        }}
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
       />
+      <AppTextInput icon="email" placeholder="Email" />
     </Screen>
   );
 }
